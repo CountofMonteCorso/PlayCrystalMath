f6ede4af2a8c8a44a1fe0cf290992e5c871b7450
Kevin <kcorso89@gmail.com>
2017-10-30 22:02:29 -0700

    submit before i forget

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,56 @@
-$(document).onReady(function() {
+$(document).ready(function() {
 	// all your code here
-	var yourMagicNumber = 0;
-	var randomNumber = randomNumGen();
+	var yourSum = 0;
+	var randomNumber;
 	var wins = 0;
 	var losses = 0;
-	var drugz
+	var meth;
+	var coke;
+	var ecs;
+	var weed;
+function game (){
 
-	function randomNumDrugz() {
-		return {
-			meth: {
-				points: Math.floor(Math.random() * 12) + 1;
-				imageURL: "../images/meth.jpg"
-			},
-			weed: {
-				points: Math.floor(Math.random() * 12) + 1;
-				imageURL: "../images/weed.jpg"
-			},
-			ecstasy: {
-				points: Math.floor(Math.random() * 12) + 1;
-				imageURL: "../images/ecstasy.jpg"
-			},
-			coke: {
-				points: Math.floor(Math.random() * 12) + 1;
-				imageURL: "../images/coke.jpg"
+	randomNumber = Math.floor((Math.random() * 120) + 19);
+	$("#GoalNumber").html("<p> Your number to match is: </p> <h1>"+ randomNumber + "</h1>");
+
+	var meth = Math.floor((Math.random() * 11) + 2);
+	$("#meth").attr("value", meth);
+
+	var coke = Math.floor((Math.random() * 11) + 2);
+	$("#coke").attr("value", coke);
+
+	var ecs = Math.floor((Math.random() * 11) + 2);
+	$("#ecstasy").attr("value", ecs);
+
+	var weed = Math.floor((Math.random() * 11) + 2);
+	$("#weed").attr("value", weed);
+}
+game();
+	$("img").on("click", function(){
+		var elton = $(this).attr("value");
+		yourSum = yourSum + parseInt(elton);
+		console.log(yourSum);
+		$("#YourSum").html("<p> Your drugs currently total: </p> <h1>" + yourSum + "</h1>");
+			if (randomNumber < yourSum) {
+				alert("you suuuuuuck lol");
+				losses++;
+				$("#L").html("Losses: " + losses);
+				yourSum = 0;
+				$("#YourSum").html("<p> Your drugs currently total: </p> <h1>" + yourSum + "</h1>");
+				game();
+
 			}
-		}
-	}
+			else if (randomNumber === yourSum) {
+				alert("lucky guess... bitch");
+				wins++;
+				$("#W").html("Wins: " + wins);
+				yourSum = 0;
+				$("#YourSum").html("<p> Your drugs currently total: </p> <h1>" + yourSum + "</h1>");
+				game();
+			}
 
-	function randomNumGen() {
-		return Math.floor(Math.random() * 119) + 30;
-	}
+	})
 
-	function setGame () {
-		yourMagicNumber = 0;
-		drugz = randomNumDrugz();
-		randomNumber = randomNumGen(); 
-		$("#GoalNumber").text(randomNumber)
-	}
-	// updateDom(didUserWin)
 
-	function renderDrugz() {
-		for (var key in drugz) {
-			var drugDiv = $("<div class='drugz-button' dataname='" + key +"'>")
-			var drugImg = $("<img alt='image' class='drug-img'>").attr("src", drugz[key].imageURL)
-			drugDiv.append(drugImg)
-			$("#drug-area").append(drugDiv)
-		}
-	}
 
-	// updateMagicNumber(crystal)
-
-	// renderMagicNumber()
 });
